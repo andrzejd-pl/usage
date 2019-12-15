6398b31c7bff7af141d4f70872d5e18364b84fdf
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-12-15 19:53:44 +0100

    add creating configuration from reader

---
+++ configuration.go
@@ -0,0 +1,23 @@
+package usage
+
+import (
+	"bytes"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+func NewConfigurationFromReader(stream *bytes.Buffer, configuration interface{}) (interface{}, error) {
+	yamlData, err := ioutil.ReadAll(stream)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(yamlData, configuration)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return configuration, err
+}
