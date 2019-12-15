d73295999e83ef675acb6a1cb836f183f6ab0711
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-12-15 19:53:08 +0100

    expand test creating configuration

--- configuration_test.go
+++ configuration_test.go
@@ -12,15 +12,59 @@ func TestNewConfigurationFromReader(t *testing.T) {
 		Test2 string `yaml:"test2"`
 		Test3 string `yaml:"test3"`
 	}
-	stream := bytes.NewBufferString("")
 
-	config, err := NewConfigurationFromReader(stream, &yamlConfig{})
-
-	if err != nil {
-		t.Errorf("code return error, but did not want")
+	testSet := []struct {
+		testName  string
+		arguments string
+		wantError bool
+		want      interface{}
+	}{
+		{
+			"empty test",
+			"",
+			false,
+			&yamlConfig{},
+		},
+		{
+			"test with error",
+			"test1:1\ntest2:2 test3:3",
+			true,
+			&yamlConfig{
+				Test1: "1",
+				Test2: "2",
+				Test3: "3",
+			},
+		},
+		{
+			"correct test",
+			"test1: 1\ntest2: 2\ntest3: 3\n",
+			false,
+			&yamlConfig{
+				Test1: "1",
+				Test2: "2",
+				Test3: "3",
+			},
+		},
 	}
 
-	if !reflect.DeepEqual(config, &yamlConfig{}) {
-		t.Errorf("got %v want %v", config, yamlConfig{})
+	for _, testData := range testSet {
+		t.Run(testData.testName, func(t *testing.T) {
+			stream := bytes.NewBufferString(testData.arguments)
+			config, err := NewConfigurationFromReader(stream, &yamlConfig{})
+
+			if err != nil {
+				if testData.wantError {
+					return
+				}
+
+				t.Errorf("code return error, but did not want it %v", err)
+			} else if testData.wantError {
+				t.Errorf("code did not return error, but want it")
+			}
+
+			if !reflect.DeepEqual(config, testData.want) {
+				t.Errorf("got %v want %v", config, testData.want)
+			}
+		})
 	}
 }
